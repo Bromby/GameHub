ff07b048c36cb4d6987121d135054057d9654465
brombactor <mark@greenkite.com>
2023-11-21 10:41:14 -0500

    Skeleton Container

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -3,21 +3,27 @@ import {SimpleGrid, Text} from '@chakra-ui/react'
 import useGames from '../hooks/useGames'
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 
 const GameGrid = () => {
 
 const {games, error, isLoading} = useGames();
-const skeletons = [1,2,3,4,5,6];
+const skeletons = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
  
   return (
     <>
     {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm: 1, md:3, lg:4, xl: 6}} spacing={10} padding='15px'>
-      {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+    <SimpleGrid columns={{sm: 1, md:3, lg:4, xl: 5}} spacing={10} padding='15px'>
+      {isLoading && skeletons.map(skeleton =>( 
+      <GameCardContainer>
+        <GameCardSkeleton key={skeleton}/>
+        </GameCardContainer>))}
       {games.map((game) => (
-        <GameCard game={game} key={game.id}/>
+        <GameCardContainer>
+          <GameCard game={game} key={game.id}/>
+        </GameCardContainer>
       ))}
     </SimpleGrid>
     </>
