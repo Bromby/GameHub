3776cd166227672ab4f6d0af7aacbc02103ae2d6
brombactor <mark@greenkite.com>
2023-11-22 10:52:29 -0500

    genre cleanup

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -20,8 +20,8 @@ const GenreList = ({selectedGenre, onSelectGenre} : Props) => {
         {data.map((genre) => 
         <ListItem key={genre.id} paddingY={2}>
             <HStack>
-                <Image boxSize='40px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } fontSize='lg' variant='link' onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
+                <Image boxSize='40px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} objectFit='cover'/>
+                <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } fontSize='lg' variant='link' onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
 
             </HStack>
         </ListItem>)}
